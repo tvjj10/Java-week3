79455931ec5b3a14c96d74eca7648ce6a18e9f9a
Tyler Johnson <tvjj10@gmail.com>
2023-05-25 12:48:04 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        ask user how many temps they want to enter between 1-7 for days of week
+
+        if user puts neg$ or num > 7: display error
+        else
+            depenging on value
+            ener temp for mon
+            enter temp for tues
+            enter temp for wed
+
+            output average temp
+            depending on temp for each day
+                tell user what top they should be wearing
+
+            what to wear determined by the following:
+            < 0: winter coat
+            btwn: 0 - 10:light coat
+            btwn: 10-20: tshirt
+
+                enter # of days of the week you want to enter?
+
+                -1
+                    error
+                 3
+                    enter temp for mon
+                        10
+                    enter temp for tues
+                        20
+                    enter temp for wed
+                        -3
+
+                        the average temp is 9 degrees c
+
+                        on monday, ypu should
+         */
+
+
+    }
+    static void eample1() {
+
+        /*
+        variables?
+        daysEntered : int
+        daysOfWeek:array of strings
+        whatToWearOptions: array of strings
+        tempResponses: array of int/double
+        averageTemp: double
+        decisionResponse: array of strings based on temp
+         */
+        Scanner uInput = new Scanner();
+        int temp;
+    }
+}
